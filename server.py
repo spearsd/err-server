e617b52ec53d65dacc0fa30e1eaf1bbcf3138fa7
Dustin <dustin.spears@live.com>
2017-12-08 13:13:05 -0500

    Update server.py

--- server.py
+++ server.py
@@ -13,7 +13,7 @@ class AutoSysServer(BotPlugin):
             proc.wait()
             file.seek(0)
             target_server = str(target_server) + str(file.read())
-        return target_server + " is now the targeted server."
+        return "Targeted server: " + target_server
     
     @botcmd
     def server_active(self, msg, args):
