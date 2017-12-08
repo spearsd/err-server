ddad8d5d71d1558154d6683cbc844eb419b5313b
Dustin <dustin.spears@live.com>
2017-12-08 11:46:45 -0500

    Update server.py

--- server.py
+++ server.py
@@ -3,13 +3,17 @@ import subprocess, tempfile, re, time
 
 class AutoSysServer(BotPlugin):
     """AutoSys server plugin for Errbot"""
-    target_server = ""
 
     @botcmd
     def server_target(self, msg, args):
         """Target server for jobs"""
-        self.target_server = args
-        return args + " is now the targeted server."
+        target_server = ""
+        with tempfile.TemporaryFile() as tempf:
+            proc = subprocess.Popen(['echo',args,'>','/var/errbot/target_server'], stdout=tempf)
+            proc.wait()
+            tempf.seek(0)
+            target_server = str(target_server) + str(tempf.read())
+        return target_server + " is now the targeted server."
     
     @botcmd
     def server_active(self, msg, args):
